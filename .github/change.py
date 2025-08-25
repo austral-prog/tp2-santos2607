845bcadda22d63070d336e9aa3f7960e1d0e7043
santos2607 <ssacchetti@mail.austral.edu.ar>
2025-08-25 11:58:10 -0300

    Add files via upload

---
+++ .github/change.py
@@ -0,0 +1,20 @@
+def change():
+    expense = 23.75
+    money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    vuelto = money - expense
+    pesos = int(vuelto)
+    print(pesos)
+    print("Centavos:")
+    centavos = int((vuelto - pesos)*100)
+    print(centavos)
+change()
+
+    

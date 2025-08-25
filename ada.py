cf4d5320d7ece23367b45678445857cbcd7160c0
santos2607 <ssacchetti@mail.austral.edu.ar>
2025-08-25 11:53:36 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,9 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    print(first_name.lower(), last_name.lower())
+     print(first_name.lower(), last_name.lower())
     print(first_name.capitalize(), last_name.capitalize())
     print(first_name.upper(), last_name.upper())
     print("\t" + first_name.lower(), last_name.lower())
 
 ada()
+
+    
